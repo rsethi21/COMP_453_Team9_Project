9929c001db696a548de65be6f680eaf860cd23e5
Channah Naiman <cnaiman@luc.edu>
2022-05-01 13:39:22 -0500

    updated models.py

--- Lab4c-Rohan Sethi, Viviana Pautsch, Hoang Nyguen/flaskDemo/models.py
+++ Lab4c-Rohan Sethi, Viviana Pautsch, Hoang Nyguen/flaskDemo/models.py
@@ -16,56 +16,61 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+#    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+#    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"User('{self.username}', '{self.email}')"
 
 
-class Post(db.Model):
-     __table_args__ = {'extend_existing': True}
-     id = db.Column(db.Integer, primary_key=True)
-     title = db.Column(db.String(100), nullable=False)
-     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-     content = db.Column(db.Text, nullable=False)
-     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#class Post(db.Model):
+#     __table_args__ = {'extend_existing': True}
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(100), nullable=False)
+#     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+#     content = db.Column(db.Text, nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-     def __repr__(self):
-         return f"Post('{self.title}', '{self.date_posted}')"
-
-
-
+#     def __repr__(self):
+#         return f"Post('{self.title}', '{self.date_posted}')"
 
 
 
-class Dependent(db.Model):
-    __table__ = db.Model.metadata.tables['dependent']
+
+
+
+class Accepts(db.Model):
+    __table__ = db.Model.metadata.tables['accepts']
     
-class Department(db.Model):
-    __table__ = db.Model.metadata.tables['department']
+class Doctor(db.Model):
+    __table__ = db.Model.metadata.tables['doctor']
 
 # used for query_factory
-def getDepartment(columns=None):
-    u = Department.query
-    if columns:
-        u = u.options(orm.load_only(*columns))
-    return u
+#def getDepartment(columns=None):
+#    u = Department.query
+#    if columns:
+#        u = u.options(orm.load_only(*columns))
+#    return u
 
-def getDepartmentFactory(columns=None):
-    return partial(getDepartment, columns=columns)
+#def getDepartmentFactory(columns=None):
+#    return partial(getDepartment, columns=columns)
 
-class Dept_Locations(db.Model):
-    __table__ = db.Model.metadata.tables['dept_locations']
+#class Dept_Locations(db.Model):
+#    __table__ = db.Model.metadata.tables['dept_locations']
     
-class Employee(db.Model):
-    __table__ = db.Model.metadata.tables['employee']
-class Project(db.Model):
-    __table__ = db.Model.metadata.tables['project']
-class Works_On(db.Model):
-    __table__ = db.Model.metadata.tables['works_on']
-
+class Insurance(db.Model):
+    __table__ = db.Model.metadata.tables['insurance']
+class InsuredBy(db.Model):
+    __table__ = db.Model.metadata.tables['insuredby']
+class Patient(db.Model):
+    __table__ = db.Model.metadata.tables['patient']
+class Person(db.Model):
+    __table__ = db.Model.metadata.tables['person']
+class TreatedBy(db.Model):
+    __table__ = db.Model.metadata.tables['treatedby']
+class Treatment(db.Model):
+    __table__ = db.Model.metadata.tables['treatment']
     
 
   
